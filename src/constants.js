e6f5d7f51759ad7b2fee41471c3a44540d0a73ce
Dip Bhattacharjee <dip.bhattacharjee@postman.com@Dips-MacBook-Pro-2.local>
2024-09-05 08:41:25 +0530

    added constant file

---
+++ src/constants.js
@@ -0,0 +1,6 @@
+export const EMPTY_CONTENT = 'empty_content';
+export const INVALID_CONTENT = 'invalid_content';
+export const INVALID_STRUCTURE = 'invalid_structure';
+export const UNSUPPORTED_NODE = 'unsupported_node';
+export const TEXT_NODE_TYPE = 'text';
+export const SLATE_DATA = 'slate_data';
